15b0ae502df3ff6996514ceffe1ca23b42528477
Liam Gensel <liamgens@gmail.com>
2021-04-08 11:04:24 -0600

    basic movement with placeholder

---
+++ Assets/Scripts/Caesar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caesar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+        float speed = 10f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = 1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1f;
+        }
+        transform.position += new Vector3(moveX, moveY) * speed * Time.deltaTime;
+    }
+}
